2dbe35d650f7ee493d5f5ca2e296977fac3d9945
gozzafadillah <gozzafadillah@gmail.com>
2023-10-18 21:42:28 +0700

    slice/class: added logo in hero

---
+++ src/pages/Admin.page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeadingBox from "../components/HeadingBox";
+import { Box } from "@chakra-ui/react";
+
+const AdminPage = () => {
+  return (
+    <>
+      <HeadingBox>Admin - Admin Page</HeadingBox>
+      <Box minH="100vh"></Box>
+    </>
+  );
+};
+
+export default AdminPage;
