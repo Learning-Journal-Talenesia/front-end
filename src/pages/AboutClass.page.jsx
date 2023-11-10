985356d42379c6a65d120f1a39b449c9202191d0
rafi tanujaya <rafibussines@gmail.com>
2023-11-10 17:03:31 +0700

    feat(slice): menambahkan slice Page AboutClass

---
+++ src/pages/AboutClass.page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AboutClassView from '../views/AboutClass.view'
+
+const AboutClassPage = () => {
+  return (
+    <AboutClassView />
+  )
+}
+
+export default AboutClassPage
